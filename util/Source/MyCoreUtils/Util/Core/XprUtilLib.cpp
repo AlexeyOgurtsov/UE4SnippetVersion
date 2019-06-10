b530c9aa58ab762adb144197ff81be5109e764bd
alexey_eng <alexey_eng@mail.ru>
2019-06-10 19:16:40 +0500

    Subtree changed, committing it

--- util/Source/MyCoreUtils/Util/Core/XprUtilLib.cpp
+++
@@ -1,7 +0,0 @@
-#include "XprUtilLib.h"
-#include "Logging/LogMacros.h"
-
-void UXprUtilLib::LogByCategory(const FLogCategoryBase& InLogCategory, ELogVerbosity::Type InVerbosity, const FString& InString)
-{
-	//UE_LOG(InLogCategory, Log, TEXT("Test"));
-}
